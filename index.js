89ccc9e95c3593ab7500498049572ef4b9281bff
liutsing <liutsingluo@163.com>
2018-11-26 16:13:55 +0800

    ~

--- index.js
+++ index.js
@@ -2,26 +2,35 @@ const fetch = require("node-fetch");
 const url = "http://open.douyucdn.cn/api/RoomApi/room/309923";
 const schedule = require("node-schedule");
 const fs = require("fs");
-let flag = true;
+const redis = require("redis"),
+  client = redis.createClient();
 
-const j = schedule.scheduleJob("*/2 10-23 * * *", fireDate => {
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", err => console.error("Error " + err));
+client.set("live_flag", true, redis.print);
+
+const j = schedule.scheduleJob("*/20 10-23 * * *", fireDate => {
   let date = new Date();
-  let log = `This job was supposed to run at ${fireDate}, but actually ran at ${date}`;
+  let log = `计划运行时间：${fireDate.toLocaleTimeString()}, 实际运行时间: ${date.toLocaleTimeString()}`;
   let file = `./log/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}.log`;
   write(file, log);
 
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      console.log({ data });
       if (data.error === 0) {
         let obj = data.data;
         if (obj.room_status === "1") {
-          flag = false;
-          console.log("已开播");
+          if (client.get("live_flag")) {
+            //提醒一次
+            console.log("已开播");
+            console.log("发送邮件"); //回调设置 flag=false
+            client.set("live_flag", false);
+          }
         } else if (obj.room_status === "2") {
-          flag = true;
-          console.log("未开播");
+          client.set("live_flag", true);
         }
       } else {
         write(file, data);
@@ -29,10 +38,6 @@ const j = schedule.scheduleJob("*/2 10-23 * * *", fireDate => {
     })
     .catch(e => write(file, e.getMessage()));
 });
-
-if (!flag) {
-  j.cancel();
-}
 
 const write = (file, data) => {
   fs.writeFile(
